69262612e95b31ce8654dce553f43c2b3ca2dec3
valu <mzj@jundie.net>
2017-05-13 13:02:07 +0800

    升级

--- src/jundie.net.core_pager/HtmlHelper.cs
+++ src/jundie.net.core_pager/HtmlHelper.cs
@@ -10,18 +10,20 @@ namespace jundie.net.core_pager
     {
         public static IHtmlContent PagedListPager(this IHtmlHelper html,
                                                    IPageList list,
-                                                   Func<int, string> generatePageUrl)
+                                                   Func<int, string> generatePageUrl,
+                                                   string prev_page_text = "上一页",
+                                                   string next_page_text = "下一页")
         {
 
             TagBuilder ul = new TagBuilder("ul");
             ul.AddCssClass("pagination");
-            ul = CompleteUlBefore(ul, list, generatePageUrl);
+            ul = CompleteUlBefore(ul, list, generatePageUrl, prev_page_text);
             for (int i = 1; i <= list.PageCount; i++)
             {
                 string temp = generatePageUrl(i);
                 ul.InnerHtml.AppendHtml(GenerateItem(temp, i, list.PageNumber));
             }
-            ul = CompleteUlAfter(ul, list, generatePageUrl);
+            ul = CompleteUlAfter(ul, list, generatePageUrl, next_page_text);
             return ul;
         }
 
@@ -39,28 +41,28 @@ namespace jundie.net.core_pager
             return li;
         }
 
-        public static TagBuilder CompleteUlBefore(TagBuilder ul, IPageList list, Func<int, string> generatePageUrl)
+        public static TagBuilder CompleteUlBefore(TagBuilder ul, IPageList list, Func<int, string> generatePageUrl, string prev_page_text = "上一页")
         {
             if (list.PageCount >= list.PageNumber && list.PageNumber > 1)
             {
                 TagBuilder nextLi = new TagBuilder("li");
                 TagBuilder LastA = new TagBuilder("a");
                 LastA.Attributes["href"] = generatePageUrl(list.PageNumber - 1);
-                LastA.InnerHtml.SetContent("上一页");
+                LastA.InnerHtml.SetContent(prev_page_text);
                 nextLi.InnerHtml.SetHtmlContent(LastA);
                 ul.InnerHtml.AppendHtml(nextLi);
             }
             return ul;
         }
 
-        public static TagBuilder CompleteUlAfter(TagBuilder ul, IPageList list, Func<int, string> generatePageUrl)
+        public static TagBuilder CompleteUlAfter(TagBuilder ul, IPageList list, Func<int, string> generatePageUrl, string next_page_text = "下一页")
         {
             if (list.PageCount > list.PageNumber && list.PageNumber >= 1)
             {
                 TagBuilder nextLi = new TagBuilder("li");
                 TagBuilder nextA = new TagBuilder("a");
                 nextA.Attributes["href"] = generatePageUrl(list.PageNumber + 1);
-                nextA.InnerHtml.SetContent("下一页");
+                nextA.InnerHtml.SetContent(next_page_text);
                 nextLi.InnerHtml.SetHtmlContent(nextA);
                 ul.InnerHtml.AppendHtml(nextLi);
             }
